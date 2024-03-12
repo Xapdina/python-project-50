82851dc6fd636164c47346ac836e3692d9864d52
Даниил Багатурия <daniilbagaturia@Daniils-MacBook-Air.local>
2024-03-12 20:47:56 +0300

    split function parser_args

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,19 +3,6 @@ import argparse
 import json
 
 
-def parser_args(file_path1=None, file_path2=None):
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.',
-        add_help=True,
-        prog='gendiff'
-    )
-    parser.add_argument(file_path1)
-    parser.add_argument(file_path2)
-    parser.add_argument('-f', '--format', help='set format of output')
-    arg = parser.parse_args()
-    print(arg)
-
-
 def to_str(value):
     return str(value).lower()
 

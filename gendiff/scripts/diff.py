87099fd71684850360f8cb9b50a838e58b026671
Даниил Багатурия <daniilbagaturia@MacBook-Air-Daniil.local>
2024-03-13 09:28:03 +0300

    nothing changes

--- gendiff/scripts/diff.py
+++ gendiff/scripts/diff.py
@@ -5,5 +5,5 @@ from gendiff.gendiff import generate_diff
 
 def main():
     if __name__ == '__main__':
-        generate_diff('/.github/test/fixtures/file1.json',
-                      '/.github/test/fixtures/file2.json')
+        generate_diff('/.github/tests/fixtures/file1.json',
+                      '/.github/tests/fixtures/file2.json')
